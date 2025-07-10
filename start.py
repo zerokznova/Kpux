9198e9e7decd55d3f1fa4c1b867996e2e47e547d
zerokznova <manuiphones91@gmail.com>
2025-07-10 16:53:46 -0300

    Delete start.py

--- start.py
+++
@@ -1,5 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Olá! 👋\nUse /cpf, /nome, /placa e outros comandos para consultar.\nExemplo: /nome Jair Messias Bolsonaro")
