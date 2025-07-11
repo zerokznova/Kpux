0e4cf546e397c550e8273f87abfc1b777937ec08
zerokznova <manuiphones91@gmail.com>
2025-07-11 17:37:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,23 @@
-from telegram.ext import Application, CommandHandler
+from telegram.ext import Updater, CommandHandler
 from handlers.start import start
-from handlers.consultas import (
-    cpf, cnpj, nome, rg, cpfsimpl, rgsimpl, pis, titulo,
-    telefone, email, cns, mae, pai, placa, chassi,
-    renavam, motor, fotorj, fotosp, funcionarios, razao
-)
+from handlers.consultas import cpf, cnpj, placa, nome
 
-TOKEN = "7794910663:AAFvwakiaQ3muTSClhDfNa3rRN78YTD-ArU"
+TOKEN ="7794910663:AAFvwakiaQ3muTSClhDfNa3rRN78YTD-ArU"
 
 def main():
-    app = Application.builder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("cpf", cpf))
-    app.add_handler(CommandHandler("cnpj", cnpj))
-    app.add_handler(CommandHandler("nome", nome))
-    app.add_handler(CommandHandler("rg", rg))
-    app.add_handler(CommandHandler("cpfsimpl", cpfsimpl))
-    app.add_handler(CommandHandler("rgsimpl", rgsimpl))
-    app.add_handler(CommandHandler("pis", pis))
-    app.add_handler(CommandHandler("titulo", titulo))
-    app.add_handler(CommandHandler("telefone", telefone))
-    app.add_handler(CommandHandler("email", email))
-    app.add_handler(CommandHandler("cns", cns))
-    app.add_handler(CommandHandler("mae", mae))
-    app.add_handler(CommandHandler("pai", pai))
-    app.add_handler(CommandHandler("placa", placa))
-    app.add_handler(CommandHandler("chassi", chassi))
-    app.add_handler(CommandHandler("renavam", renavam))
-    app.add_handler(CommandHandler("motor", motor))
-    app.add_handler(CommandHandler("fotorj", fotorj))
-    app.add_handler(CommandHandler("fotosp", fotosp))
-    app.add_handler(CommandHandler("funcionarios", funcionarios))
-    app.add_handler(CommandHandler("razao", razao))
+updater = Updater(TOKEN, use_context=True)
+dp = updater.dispatcher
 
-    print("🤖 Bot rodando...")
-    app.run_polling()
+dp.add_handler(CommandHandler("start", start))  
+dp.add_handler(CommandHandler("cpf", cpf))  
+dp.add_handler(CommandHandler("cnpj", cnpj))  
+dp.add_handler(CommandHandler("placa", placa))  
+dp.add_handler(CommandHandler("nome", nome))  
 
-if __name__ == "__main__":
-    main()
+print("🤖 Bot rodando...")  
+updater.start_polling()  
+updater.idle()
+
+if name == "main":
+main()
+
