7c1cc75ff18994d9b8a534c04e1005ae0467c5b3
zerokznova <manuiphones91@gmail.com>
2025-07-11 17:51:50 -0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,5 +1,5 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Olá! 👋\nUse /cpf, /nome, /placa e outros comandos para consultar.\nExemplo: /nome Jair Messias Bolsonaro")
+async def start(update, context):
+    await update.message.reply_text(
+        "Olá! 👋\nUse /cpf, /nome, /placa e outros comandos para consultar.\n"
+        "Exemplo: /nome Jair Messias Bolsonaro"
+    )
