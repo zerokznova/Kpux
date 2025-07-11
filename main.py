5ad16f6cd2d683d9a9f7435a41cb2258285786a6
zerokznova <manuiphones91@gmail.com>
2025-07-11 17:49:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import ApplicationBuilder, CommandHandler
 from handlers.start import start
 from handlers.consultas import cpf, cnpj, placa, nome
 
-TOKEN ="7794910663:AAFvwakiaQ3muTSClhDfNa3rRN78YTD-ArU"
+TOKEN = "7794910663:AAFvwakiaQ3muTSClhDfNa3rRN78YTD-ArU"
 
 def main():
-updater = Updater(TOKEN, use_context=True)
-dp = updater.dispatcher
+    app = ApplicationBuilder().token(TOKEN).build()
 
-dp.add_handler(CommandHandler("start", start))  
-dp.add_handler(CommandHandler("cpf", cpf))  
-dp.add_handler(CommandHandler("cnpj", cnpj))  
-dp.add_handler(CommandHandler("placa", placa))  
-dp.add_handler(CommandHandler("nome", nome))  
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("cpf", cpf))
+    app.add_handler(CommandHandler("cnpj", cnpj))
+    app.add_handler(CommandHandler("placa", placa))
+    app.add_handler(CommandHandler("nome", nome))
 
-print("🤖 Bot rodando...")  
-updater.start_polling()  
-updater.idle()
+    print("🤖 Bot rodando...")
+    app.run_polling()
 
-if name == "main":
-main()
-
+if __name__ == "__main__":
+    main()
